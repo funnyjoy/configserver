60b26f610b3b165ca1ab3c2571abdf946b8035a5
funnyjoy <funnyjoy@P00074430-002.CNS.LGCNS.COM>
2020-06-29 13:58:03 +0900

    config server 소스 커밋

---
+++ src/test/java/com/jpetstore/config/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpetstore.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
